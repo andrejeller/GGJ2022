c6db5a535f49a296109ab41afbefae0594dfdd80
andrejeller <jeller.andre@gmail.com>
2022-01-16 22:22:05 -0300

    Começando a Ideia

---
+++ GGJ2022/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour {
+
+    public float testeS = 30.0f;
+    public float testeJ = 80.0f;
+
+
+    private Rigidbody2D myBody;
+    //private float speed = testeS;
+    //private float jumpForce = testeJ;
+
+    void Start() {
+        myBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update() {
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x * testeS;
+        myBody.velocity = new Vector2(dir.x, myBody.velocity.y);
+        
+    }
+
+    public void Jump() {
+        myBody.AddForce(Vector2.up * testeJ, ForceMode2D.Impulse);
+    }
+}
