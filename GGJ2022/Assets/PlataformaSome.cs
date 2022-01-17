44be0c0b586625b83a81346287885240e96a68d2
andrejeller <jeller.andre@gmail.com>
2022-01-17 14:57:04 -0300

    Testando/Programando mecaninca

---
+++ GGJ2022/Assets/PlataformaSome.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaSome: MonoBehaviour {
+
+    public SpriteRenderer sprite;
+    public PlatformEffector2D platform;
+
+    [Space] public LayerMask ativado;
+    public LayerMask desativado;
+
+    void Start() {
+        //sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            platform.colliderMask = ativado;
+            Debug.Log("Ativado");
+            
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            platform.colliderMask = desativado;
+            Debug.Log("Desativado");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.gameObject.name);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(collision.gameObject.name);
+    }
+
+}
