da9116f694cc7712dec3126eb29c2ba2d97cf840
andrejeller <jeller.andre@gmail.com>
2022-01-18 15:50:45 -0300

    Fundo+Camera

---
+++ GGJ2022/Assets/SeguirPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+    void Update() {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
