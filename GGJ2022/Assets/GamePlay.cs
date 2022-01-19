8631555f3bd31a68488c7a4331d2c7dee76d101d
andrejeller <jeller.andre@gmail.com>
2022-01-19 14:40:34 -0300

    Final

--- GGJ2022/Assets/GamePlay.cs
+++ GGJ2022/Assets/GamePlay.cs
@@ -5,6 +5,13 @@ using DG.Tweening;
 
 public class GamePlay : MonoBehaviour {
 
+
+    public static GamePlay instance;
+    private void Awake() {
+        if (instance == null) instance = this;
+        else Destroy(this);
+    }
+
     public GameObject FundoPreto, Seta;
     private int ladoFundoPreto = -1;
 
@@ -31,5 +38,10 @@ public class GamePlay : MonoBehaviour {
         FundoPreto.transform.DOMoveX(goTo, 0.5f);
         Seta.transform.DOLocalMoveX(seta_goTo, 0.6f);
 
+    }
+
+
+    public void FimDeJogo() {
+        Debug.Log("Fim!");
     }
 }
