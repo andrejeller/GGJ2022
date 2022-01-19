d212ae540301556c4306320832b753034da2d50c
andrejeller <jeller.andre@gmail.com>
2022-01-19 13:27:02 -0300

    LogoPlaceholder + FaseCompleta

---
+++ GGJ2022/Assets/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class MainMenu: MonoBehaviour {
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+
+
+    // Botoes
+    public void Info() {
+
+    }
+
+    public void RankingGooglePlay() {
+
+    }
+
+
+    public void PlayGame() {
+        //Faz uma Animaçao
+        //Ai muda a cena
+        SceneManager.LoadScene(1);
+    }
+
+}
