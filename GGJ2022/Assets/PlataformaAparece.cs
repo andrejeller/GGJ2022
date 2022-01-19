0c0a4b7f293e28fd46995f73f518fc82ecd293fb
andrejeller <jeller.andre@gmail.com>
2022-01-19 14:28:44 -0300

    FaseOkay

---
+++ GGJ2022/Assets/PlataformaAparece.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaAparece : MonoBehaviour {
+
+    public GameObject rampa;
+
+    private void Start() {
+        rampa.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            rampa.SetActive(true);
+        }
+    }
+}
---
+++ GGJ2022/Assets/Roda.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roda : MonoBehaviour {
+
+    public GameObject a, b, c, d;
+    public float lado = -1;
+    public float j = 5.0f;
+
+    void Update() {
+        float vel = j * Time.timeScale * lado;
+        transform.Rotate(new Vector3(0, 0, 1), vel);
+        a.transform.Rotate(new Vector3(0, 0, 1), -vel);
+        b.transform.Rotate(new Vector3(0, 0, 1), -vel);
+        c.transform.Rotate(new Vector3(0, 0, 1), -vel);
+        d.transform.Rotate(new Vector3(0, 0, 1), -vel);
+    }
+}
